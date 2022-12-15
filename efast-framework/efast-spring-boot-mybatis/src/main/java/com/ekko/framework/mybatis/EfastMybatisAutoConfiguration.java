dd240c564033f292ecf1759b7299d9060d25136c
lixiaolong3 <lixiaolong3@xiaomi.com>
2022-12-15 19:21:24 +0800

    feat:第一次提交

---
+++ efast-framework/efast-spring-boot-mybatis/src/main/java/com/ekko/framework/mybatis/EfastMybatisAutoConfiguration.java
@@ -0,0 +1,10 @@
+package com.ekko.framework.mybatis;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis自动配置类
+ */
+@Configuration(proxyBeanMethods = false)
+public class EfastMybatisAutoConfiguration {
+}
---
+++ efast-framework/efast-spring-boot-mybatis/src/main/java/com/ekko/framework/mybatis/config/EfastMybatisPlusConfiguration.java
@@ -0,0 +1,30 @@
+package com.ekko.framework.mybatis.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ekko
+ * mybatis-plus配置类
+ */
+@Configuration(proxyBeanMethods = false)
+@AutoConfigureAfter(MybatisPlusAutoConfiguration.class)
+public class EfastMybatisPlusConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
